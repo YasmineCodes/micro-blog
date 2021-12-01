9efd4fe48b01ea86c6f6532f169eacf7d4b22df9
yassminat <yasminekamel1@gmail.com>
2021-12-01 18:29:17 -0500

    created home, navbar, bloglist components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1 className="title">Blog It</h1>
+            <div className="navlinks">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
