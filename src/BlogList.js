9db51f9093bb2bf126a2cb9dcb8dbd86cca530c3
yassminat <yasminekamel1@gmail.com>
2021-12-09 15:01:38 -0500

    updated home and bloglist

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,10 @@
-const BlogList = ({ blogs, handleDelete}) => {
+const BlogList = ({ blogs}) => {
     return ( 
         <div className="blog-list">
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>by {blog.author}</p>
-                    <button onClick={()=>handleDelete(blog.id)}>Delete</button>
                 </div>
             ))}
         </div>   
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
     
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true); 
+    const [error, setError] = useState(null); 
     
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id); 
-        setBlogs(newBlogs); 
-    }
     
+    
+    // useeffect used to run a function everytime the component renders 
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Couldn't fetch data for that resource.")
+                }
+                return res.json(); 
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+                setError(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+    }, []); 
     
     return (
         <div className="home">
-            <BlogList blogs={blogs} handleDelete={handleDelete}/>
+            {error && <div> {error} </div> }
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} />}
         </div>
      );
 }
